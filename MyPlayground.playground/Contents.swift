86073a88251da396dd240953c9f2d40bcb3c44cc
treborsz <rzabala@pagustech.com>
2016-01-30 09:02:54 -0300

    primer commité

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var rango = 0...100
+
+for numero in rango {
+    
+    if numero % 5 == 0
+    {
+         print ( "# \(numero) Bingo!!!" )
+    }
+    else if numero % 2 == 0
+    {
+        print ( "# \(numero) par")
+    }
+    else if numero % 2 == 1
+    {
+        print ("# \(numero) impar!!!")
+    }
+    
+    if numero > 30 && numero < 40
+    {
+        print ( "# \(numero) Viva Swift!!!")
+    }
+
+}
